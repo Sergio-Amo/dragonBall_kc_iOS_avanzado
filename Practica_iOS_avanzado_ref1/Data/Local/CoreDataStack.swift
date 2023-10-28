f5c41277435d6b1ccdc5f4245488f9aac6d9163b
Sergio Amo <sdarillo@gmail.com>
2023-10-28 19:48:45 +0200

    CoreData save

---
+++ Practica_iOS_avanzado_ref1/Data/Local/CoreDataStack.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataStack.swift
+//  Practica_iOS_avanzado_ref1
+//
+//  Created by Sergio Amo on 28/10/23.
+//
+
+import Foundation
---
+++ Practica_iOS_avanzado_ref1/Domain/Models/HeroLocation.swift
@@ -0,0 +1,10 @@
+//
+//  HeroLocation.swift
+//  Practica_iOS_avanzado_ref1
+//
+//  Created by Sergio Amo on 28/10/23.
+//
+
+import Foundation
+
+
