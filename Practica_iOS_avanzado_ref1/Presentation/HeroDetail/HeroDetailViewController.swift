7773bc10e0b6fc7576f1fb4b733ed86121b72a80
Sergio Amo <sdarillo@gmail.com>
2023-10-29 11:50:24 +0100

    Added HeroDetail interface and file structure

---
+++ Practica_iOS_avanzado_ref1/Presentation/HeroDetail/HeroDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeroDetailViewController.swift
+//  Practica_iOS_avanzado_ref1
+//
+//  Created by Sergio Amo on 29/10/23.
+//
+
+import UIKit
+
+class HeroDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Practica_iOS_avanzado_ref1/Presentation/HeroDetail/HeroDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HeroDetailViewModel.swift
+//  Practica_iOS_avanzado_ref1
+//
+//  Created by Sergio Amo on 29/10/23.
+//
+
+import Foundation
