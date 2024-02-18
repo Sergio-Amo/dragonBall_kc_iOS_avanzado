9277dd0b9c7b6fcdc61c34d2f22ab95986ddb361
Sergio Amo <sergio.amo@outlook.com>
2024-02-18 20:32:31 +0100

    Remove delay on splash screen

--- Practica_iOS_avanzado_ref1/Presentation/Splash/SplashViewModel.swift
+++ Practica_iOS_avanzado_ref1/Presentation/Splash/SplashViewModel.swift
@@ -36,7 +36,7 @@ final class SplashViewModel: SplashViewControllerDelegate {
     // MARK: - Public functions -
     func onViewAppear() {
         viewState?(.loading(true))
-        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(1)) { [weak self] in
+        DispatchQueue.global().async { [weak self] in
             self?.tokenExists == true ? self?.viewState?(.navigateToHeroes) : self?.viewState?(.navigateToLogin)
         }
     }
